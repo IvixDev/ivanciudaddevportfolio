354c3f93e9c2631e86393a1626f57e4e281543e9
Srivix <ivanciudad98@gmail.com>
2024-01-05 22:58:10 +0100

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+		screens: {
+			'mobile': '0px',
+			'tablet': '640px',
+			'laptop': '1024px',
+			'desktop': '1280px',
+			'bigDesktop': '1536px',
+		},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
