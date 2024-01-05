ddff067ed27f5afaa7b5a0df47c1758e5098a71f
Srivix <ivanciudad98@gmail.com>
2024-01-06 00:22:37 +0100

    check

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     analytics: true,
     speedInsights: {
