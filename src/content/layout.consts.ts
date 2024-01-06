75d2c80e11a8639e1865bdc8c3db07d3f970f162
Srivix <ivanciudad98@gmail.com>
2024-01-06 11:17:24 +0100

    remove scroll and instagram

---
+++ src/content/layout.consts.ts
@@ -0,0 +1,6 @@
+export const socialMediaLinks = [
+  {href: "https://www.linkedin.com/in/iv%C3%A1n-ciudad-espinar-2195191b2/", name: "linkedin"},
+  {href: "mailto:ivanciudad98@gmail.com", name: "gmail"},
+  {href: "https://github.com/Srivix", name: "github"},
+  {href: "https://x.com/Mrivix", name: "X"},
+]
