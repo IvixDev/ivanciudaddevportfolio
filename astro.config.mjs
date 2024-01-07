977688e779a99b839e4791a83469fbd038b5ba8c
Srivix <ivanciudad98@gmail.com>
2024-01-07 15:22:15 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
   output: 'server',
+  site: 'https://ivanciudad.vercel.app',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
