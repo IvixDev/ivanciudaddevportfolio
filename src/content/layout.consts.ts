ca14ecb4d52ea11b7ebb22197994131415da2ce2
srivix <theivicrack@gmail.com>
2025-01-25 12:45:12 +0100

    Improve accesibility

--- src/content/layout.consts.ts
+++ src/content/layout.consts.ts
@@ -1,8 +1,8 @@
 export const socialMediaLinks = [
-  {href: "https://www.linkedin.com/in/iv%C3%A1n-ciudad-espinar-2195191b2/", name: "linkedin"},
-  {href: "mailto:ivanciudad98@gmail.com", name: "gmail"},
-  {href: "https://github.com/Srivix", name: "github"},
-  {href: "https://x.com/Mrivix", name: "X"},
+  {href: "https://www.linkedin.com/in/iv%C3%A1n-ciudad-espinar-2195191b2/", name: "linkedin", ariaLabel: "Linkedin"},
+  {href: "mailto:ivanciudad98@gmail.com", name: "gmail", ariaLabel: "Gmail"},
+  {href: "https://github.com/Srivix", name: "github", ariaLabel: "Github"},
+  {href: "https://x.com/Mrivix", name: "X", ariaLabel: "X"},
 ]
 
 export const headerLinks = [
