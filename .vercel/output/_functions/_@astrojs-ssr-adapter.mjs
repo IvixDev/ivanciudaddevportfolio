9bd9a056621f501aafabc0718c96e062d3b20337
srivix <theivicrack@gmail.com>
2025-01-27 14:57:57 +0100

    Remove vercel

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_BVlks-w9.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Ddo_YKhV.mjs';
-export { renderers } from '../renderers.mjs';
