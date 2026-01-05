09babc92cb0007ea79e817720f37b3de55bc068e
srivix <theivicrack@gmail.com>
2026-01-06 00:41:31 +0100

    update dependencies and new uxui style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
+import tailwind from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'never',
-  integrations: [tailwind(), icon(), sitemap()],
+  integrations: [icon(), sitemap()],
   output: 'server',
   site: 'https://www.ivanciudad.com',
   adapter: vercel({
     webAnalytics: {
       enabled: true
     }
-  })
+  }),
+  vite: {
+    plugins: [tailwind()],
+  },
 });
